dcd814623c257283b3fee1a5d959847375d90965
Manish Rathi <i.am.manish.rathi@gmail.com>
2021-12-14 20:41:12 +0100

    Day1: Initial commit

---
+++ Day1/Day1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day1
+//
+//  Created by Manish Rathi on 14/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
