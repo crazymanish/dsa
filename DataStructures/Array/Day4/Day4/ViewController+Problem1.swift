9a94a6160d7d1621c8902539f4d16a0c705989d8
Manish Rathi <i.am.manish.rathi@gmail.com>
2021-12-17 23:00:38 +0100

    Day4: initial commit

---
+++ DataStructures/Array/Day4/Day4/ViewController+Problem1.swift
@@ -0,0 +1,8 @@
+//
+//  ViewController+Problem1.swift
+//  Day4
+//
+//  Created by Manish Rathi on 17/12/2021.
+//
+
+import Foundation
---
+++ DataStructures/Array/Day4/Day4/ViewController+Problem2.swift
@@ -0,0 +1,8 @@
+//
+//  ViewController+Problem2.swift
+//  Day4
+//
+//  Created by Manish Rathi on 17/12/2021.
+//
+
+import Foundation
