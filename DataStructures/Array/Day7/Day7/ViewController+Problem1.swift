b920d6b1465088c99be1848de571120870911105
Manish Rathi <i.am.manish.rathi@gmail.com>
2021-12-20 19:39:30 +0100

    Day6: Problem1:  Print left rotation of array https://www.geeksforgeeks.org/print-left-rotation-array/

--- DataStructures/Array/Day7/Day7/ViewController+Problem1.swift
+++ DataStructures/Array/Day7/Day7/ViewController+Problem1.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+/*
+ https://www.geeksforgeeks.org/print-left-rotation-array/
+ Print left rotation of array in O(n) time and O(1) space
+
+ Given an array of size n and multiple values around which we need to left rotate the array. How to quickly print multiple left rotations?
+
+ Examples :
+
+ Input :
+ arr[] = {1, 3, 5, 7, 9}
+ k1 = 1
+ k2 = 3
+ k3 = 4
+ k4 = 6
+ Output :
+ 3 5 7 9 1
+ 7 9 1 3 5
+ 9 1 3 5 7
+ 3 5 7 9 1
+
+ Input :
+ arr[] = {1, 3, 5, 7, 9}
+ k1 = 14
+ Output :
+ 9 1 3 5 7
+ */
+
+extension ViewController {
+    func printLeftRotation(inputArray: [Int], size: Int, noOfRotation: Int) {
+        print("Input: \(inputArray.joined)")
+        print("Output after \(noOfRotation) rotations")
+
+        for index in noOfRotation..<(size+noOfRotation) {
+            print(inputArray[index%size], terminator: ", ")
+        }
+
+        print("\n")
+    }
+}
--- DataStructures/Array/Day7/Day7/ViewController.swift
+++ DataStructures/Array/Day7/Day7/ViewController.swift
@@ -18,8 +18,22 @@ class ViewController: UIViewController {
         solution2()
     }
 
-    private func solution1() {}
+    private func solution1() {
+        let inputArray = [1, 3, 5, 7, 9]
+
+        printLeftRotation(inputArray: inputArray, size: 5, noOfRotation: 1)
+        printLeftRotation(inputArray: inputArray, size: 5, noOfRotation: 3)
+        printLeftRotation(inputArray: inputArray, size: 5, noOfRotation: 4)
+        printLeftRotation(inputArray: inputArray, size: 5, noOfRotation: 6)
+        printLeftRotation(inputArray: inputArray, size: 5, noOfRotation: 14)
+    }
 
     private func solution2() {}
 }
 
+extension Array where Element == Int {
+    var joined: String {
+        let arrayAsString = map { String($0) }
+        return arrayAsString.joined(separator: ", ")
+    }
+}
