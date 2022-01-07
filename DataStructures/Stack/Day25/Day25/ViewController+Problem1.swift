196527b488d3aae46208591c2b0787eb92b82af1
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-01-07 22:21:26 +0100

    Day25: Problem1: Find maximum depth of nested parenthesis in a string https://www.geeksforgeeks.org/find-maximum-depth-nested-parenthesis-string/

--- DataStructures/Stack/Day25/Day25/ViewController+Problem1.swift
+++ DataStructures/Stack/Day25/Day25/ViewController+Problem1.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+import dsa_reusable
+
+/*
+ https://www.geeksforgeeks.org/find-maximum-depth-nested-parenthesis-string/
+ Find maximum depth of nested parenthesis in a string
+
+ We are given a string having parenthesis like below
+      “( ((X)) (((Y))) )”
+
+ We need to find the maximum depth of balanced parenthesis, like 4 in the above example. Since ‘Y’ is surrounded by 4 balanced parentheses.
+ If parenthesis is unbalanced then return -1.
+ */
+
+extension ViewController {
+    func maxParenthesisDepth(inputString: String) {
+        print("Input: \(inputString)")
+
+        let stack = Stack<String>()
+        var maxDepth = 0
+        var currentDepth = 0
+
+        for character in inputString {
+            let characterString = character.asString
+
+            if characterString == " " { continue }
+
+            if characterString == "(" {
+                maxDepth = max(maxDepth, currentDepth)
+                currentDepth = 0
+                stack.push(data: characterString)
+            } else if characterString == ")" {
+                currentDepth += 1
+
+                if stack.isEmpty { return print("Ouput: -1 (unbalanced parenthesis)") }
+
+                if stack.pop() == nil {
+                    return print("Ouput: -1 (unbalanced parenthesis)")
+                }
+            }
+        }
+
+        if stack.isEmpty == false { return print("Ouput: -1 (unbalanced parenthesis)") }
+
+        maxDepth = max(maxDepth, currentDepth)
+
+        print("Ouput: \(maxDepth)")
+    }
+}
+
+extension Character {
+    var asString: String { String(self) }
+}
--- DataStructures/Stack/Day25/Day25/ViewController.swift
+++ DataStructures/Stack/Day25/Day25/ViewController.swift
@@ -17,7 +17,14 @@ class ViewController: UIViewController {
         solution2()
     }
 
-    private func solution1() {}
+    private func solution1() {
+        maxParenthesisDepth(inputString: "(((X))(((Y))))")
+        maxParenthesisDepth(inputString: "( a(b) (c) (d(e(f)g)h) I (j(k)l)m)")
+        maxParenthesisDepth(inputString: "( p((q)) ((s)t) )")
+        maxParenthesisDepth(inputString: "")
+        maxParenthesisDepth(inputString: "b) (c) ()")
+        maxParenthesisDepth(inputString: "(b) ((c) ()")
+    }
 
     private func solution2() {}
 }
