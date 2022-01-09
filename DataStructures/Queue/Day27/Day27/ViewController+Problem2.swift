afda8b1c2f1d24f432bfd807871aadb660a56082
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-01-09 20:53:22 +0100

    Day27 Initial commit

---
+++ DataStructures/Queue/Day27/Day27/ViewController+Problem2.swift
@@ -0,0 +1,8 @@
+//
+//  ViewController+Problem2.swift
+//  Day27
+//
+//  Created by Manish Rathi on 09/01/2022.
+//
+
+import Foundation
---
+++ DataStructures/Queue/Day27/Day27/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Day27
+//
+//  Created by Manish Rathi on 09/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("--------Problem 1--------")
+        solution1()
+        print("--------Problem 2--------")
+        solution2()
+    }
+
+    private func solution1() {}
+
+    private func solution2() {}
+}
