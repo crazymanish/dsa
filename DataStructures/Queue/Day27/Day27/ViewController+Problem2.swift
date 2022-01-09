f7345ecc43101c7613c11e8b7fed1bace2f96d3d
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-01-09 21:28:33 +0100

    Day27: Problem2: Reversing a queue using recursion https://www.geeksforgeeks.org/reversing-queue-using-recursion/

--- DataStructures/Queue/Day27/Day27/ViewController+Problem2.swift
+++ DataStructures/Queue/Day27/Day27/ViewController+Problem2.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+import dsa_reusable
+
+/*
+ https://www.geeksforgeeks.org/reversing-queue-using-recursion/
+ Reversing a queue using recursion
+
+ Examples :
+ Input : Q = [5, 24, 9, 6, 8, 4, 1, 8, 3, 6]
+ Output : Q = [6, 3, 8, 1, 4, 8, 6, 9, 24, 5]
+
+ Explanation : Output queue is the reverse of the input queue.
+
+ Input : Q = [8, 7, 2, 5, 1]
+ Output : Q = [1, 5, 2, 7, 8]
+
+ Recursive Algorithm :
+ - The pop element from the queue if the queue has elements otherwise return empty queue.
+ - Call reverseQueue function for the remaining queue.
+ - Push the popped element in the resultant reversed queue.
+ */
+
+extension ViewController {
+    func reverseRecursive(inputArray: [Int]) {
+        var queue = Queue<Int>()
+
+        // Push data into Queue
+        for data in inputArray {
+            queue.enQueue(data: data)
+        }
+
+        print("Input:")
+        queue.printQueue()
+
+        queue.reverseRecursive()
+
+        print("Output:")
+        queue.printQueue()
+    }
+}
+
+extension Queue where T == Int {
+    mutating func reverseRecursive() {
+        if isEmpty { return }
+
+        let deletedItem = deQueue()!
+        reverseRecursive()
+        enQueue(data: deletedItem)
+    }
+}
--- DataStructures/Queue/Day27/Day27/ViewController.swift
+++ DataStructures/Queue/Day27/Day27/ViewController.swift
@@ -19,7 +19,11 @@ class ViewController: UIViewController {
 
     private func solution1() {
         reverse(inputArray: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+        reverse(inputArray: [1, 2, 3, 4, 5])
     }
 
-    private func solution2() {}
+    private func solution2() {
+        reverseRecursive(inputArray: [5, 24, 9, 6, 8, 4, 1, 8, 3, 6])
+        reverseRecursive(inputArray: [8, 7, 2, 5, 1])
+    }
 }
