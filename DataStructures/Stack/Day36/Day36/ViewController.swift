8cf57cad61dfc34187d1a1698538dc1c65c667c9
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-02-06 18:33:04 +0100

    Day36 Initial commit

---
+++ DataStructures/Stack/Day36/Day36/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day36
+//
+//  Created by Manish Rathi on 06/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
