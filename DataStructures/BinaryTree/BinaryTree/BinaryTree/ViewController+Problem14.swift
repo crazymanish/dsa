717fc3e90a64cb9e9d19491d4985280a0d2945c1
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-03-12 20:52:46 +0100

    Day40: 'Binary Tree Tilt' solution

---
+++ DataStructures/BinaryTree/BinaryTree/BinaryTree/ViewController+Problem14.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController+Problem14.swift
+//  BinaryTree
+//
+//  Created by Manish Rathi on 12/03/2022.
+//
+
+import Foundation
+/*
+ 563. Binary Tree Tilt
+ https://leetcode.com/problems/binary-tree-tilt/
+ Given the root of a binary tree, return the sum of every tree node's tilt.
+ The tilt of a tree node is the absolute difference between the sum of all left subtree node values and all right subtree node values. If a node does not have a left child, then the sum of the left subtree node values is treated as 0. The rule is similar if the node does not have a right child.
+
+ Example 1:
+ Input: root = [1,2,3]
+ Output: 1
+ Explanation:
+ Tilt of node 2 : |0-0| = 0 (no children)
+ Tilt of node 3 : |0-0| = 0 (no children)
+ Tilt of node 1 : |2-3| = 1 (left subtree is just left child, so sum is 2; right subtree is just right child, so sum is 3)
+ Sum of every tilt : 0 + 0 + 1 = 1
+
+ Example 2:
+ Input: root = [4,2,9,3,5,null,7]
+ Output: 15
+ Explanation:
+ Tilt of node 3 : |0-0| = 0 (no children)
+ Tilt of node 5 : |0-0| = 0 (no children)
+ Tilt of node 7 : |0-0| = 0 (no children)
+ Tilt of node 2 : |3-5| = 2 (left subtree is just left child, so sum is 3; right subtree is just right child, so sum is 5)
+ Tilt of node 9 : |0-7| = 7 (no left child, so sum is 0; right subtree is just right child, so sum is 7)
+ Tilt of node 4 : |(3+5+2)-(9+7)| = |10-16| = 6 (left subtree values are 3, 5, and 2, which sums to 10; right subtree values are 9 and 7, which sums to 16)
+ Sum of every tilt : 0 + 0 + 0 + 2 + 7 + 6 = 15
+ */
+
+extension ViewController {
+    func solve14() {
+        print("Setting up Problem14 input!")
+        let root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.right = TreeNode(3)
+
+        let output = Solution().findTilt(root)
+        print("Output: \(output)")
+    }
+}
+
+fileprivate class Solution {
+    func findTilt(_ root: TreeNode?) -> Int {
+        guard let currentNode = root else { return 0 }
+
+        let leftTilt = findTilt(currentNode.left)
+        let rightTilt = findTilt(currentNode.right)
+
+        let leftChildsSum = findSum(currentNode.left)
+        let rightChildsSum = findSum(currentNode.right)
+        let currentNodeTilt = abs(leftChildsSum-rightChildsSum)
+
+        return currentNodeTilt + leftTilt + rightTilt
+    }
+
+    private func findSum(_ root: TreeNode?) -> Int {
+        guard let currentNode = root else { return 0 }
+
+        let leftSum = findSum(currentNode.left)
+        let rightSum = findSum(currentNode.right)
+
+        let currentNodeSum = currentNode.val
+
+        return currentNodeSum + leftSum + rightSum
+    }
+}
