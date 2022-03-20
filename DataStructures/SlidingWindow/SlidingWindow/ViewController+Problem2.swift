9bcd3750a4cdffb362f4ea8685a4f13183b08a38
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-03-20 03:04:23 +0100

    ReadMe update

--- DataStructures/SlidingWindow/SlidingWindow/ViewController+Problem2.swift
+++ DataStructures/SlidingWindow/SlidingWindow/ViewController+Problem2.swift
@@ -7,24 +7,26 @@
 
 import Foundation
 /*
- 1876. Substrings of Size Three with Distinct Characters
- https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters/
- A string is good if there are no repeated characters.
- Given a string s​​​​​, return the number of good substrings of length three in s​​​​​​.
- Note that if there are multiple occurrences of the same substring, every occurrence should be counted.
- A substring is a contiguous sequence of characters in a string.
+ 1763. Longest Nice Substring
+ https://leetcode.com/problems/longest-nice-substring/
+ A string s is nice if, for every letter of the alphabet that s contains, it appears both in uppercase and lowercase. For example, "abABB" is nice because 'A' and 'a' appear, and 'B' and 'b' appear. However, "abA" is not because 'b' appears, but 'B' does not.
+ Given a string s, return the longest substring of s that is nice. If there are multiple, return the substring of the earliest occurrence. If there are none, return an empty string.
 
  Example 1:
- Input: s = "xyzzaz"
- Output: 1
- Explanation: There are 4 substrings of size 3: "xyz", "yzz", "zza", and "zaz".
- The only good substring of length 3 is "xyz".
+ Input: s = "YazaAay"
+ Output: "aAa"
+ Explanation: "aAa" is a nice string because 'A/a' is the only letter of the alphabet in s, and both 'A' and 'a' appear.
+ "aAa" is the longest nice substring.
 
  Example 2:
- Input: s = "aababcabc"
- Output: 4
- Explanation: There are 7 substrings of size 3: "aab", "aba", "bab", "abc", "bca", "cab", and "abc".
- The good substrings are "abc", "bca", "cab", and "abc".
+ Input: s = "Bb"
+ Output: "Bb"
+ Explanation: "Bb" is a nice string because both 'B' and 'b' appear. The whole string is a substring.
+
+ Example 3:
+ Input: s = "c"
+ Output: ""
+ Explanation: There are no nice substrings.
  */
 
 extension ViewController {
