1bcc61ca283d80886d92e15471908c0c27befd13
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-03-20 17:24:17 +0100

    Day48: Longest Subarray of 1's After Deleting One Element - SlidingWindow

--- DataStructures/SlidingWindow/SlidingWindow/ViewController+Problem9.swift
+++ DataStructures/SlidingWindow/SlidingWindow/ViewController+Problem9.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController+Problem7.swift
+//  ViewController+Problem9.swift
 //  SlidingWindow
 //
 //  Created by Manish Rathi on 20/03/2022.
@@ -7,67 +7,72 @@
 
 import Foundation
 /*
- 1004. Max Consecutive Ones III
- https://leetcode.com/problems/max-consecutive-ones-iii/
- Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+ 1493. Longest Subarray of 1's After Deleting One Element
+ https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/
+ Given a binary array nums, you should delete one element from it.
+ Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
 
  Example 1:
- Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
- Output: 6
- Explanation: [1,1,1,0,0,1,1,1,1,1,1]
- Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ Input: nums = [1,1,0,1]
+ Output: 3
+ Explanation: After deleting the number in position 2, [1,1,1] contains 3 numbers with value of 1's.
 
  Example 2:
- Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
- Output: 10
- Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
- Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+ Input: nums = [0,1,1,1,0,1,1,0,1]
+ Output: 5
+ Explanation: After deleting the number in position 4, [0,1,1,1,1,1,0,1] longest subarray with value of 1's is [1,1,1,1,1].
+
+ Example 3:
+ Input: nums = [1,1,1]
+ Output: 2
+ Explanation: You must delete one element.
  */
 
 extension ViewController {
-    func solve7() {
-        print("Setting up Problem7 input!")
+    func solve9() {
+        print("Setting up Problem9 input!")
 
-        var input = [1,1,1,0,0,0,1,1,1,1,0]
+        var input = [1,1,0,1]
         print("Input1: \(input)")
-        var output = Solution().longestOnes(input, 2)
+        var output = Solution().longestSubarray(input)
         print("Output1: \(output)")
 
-        input = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1]
+        input = [0,1,1,1,0,1,1,0,1]
         print("Input2: \(input)")
-        output = Solution().longestOnes(input, 3)
+        output = Solution().longestSubarray(input)
         print("Output2: \(output)")
 
-        input = [0,0,0,1]
+        input = [1,1,1]
         print("Input3: \(input)")
-        output = Solution().longestOnes(input, 4)
+        output = Solution().longestSubarray(input)
         print("Output3: \(output)")
     }
 }
 
 fileprivate class Solution {
     private var slidingWindow: SlidingWindow<[Int]>!
-    private var flipPossibleCount: Int!
+    private var canIgnoreZero = true
     private var currentWindowConsecutiveOnes = 0
     private var maximumConsecutiveOnes = Int.min
 
     private var slidingWindowFirstValue: Int { Array(slidingWindow.startIndexValue).first! }
     private var slidingWindowLastValue: Int { Array(slidingWindow.endIndexValue).first! }
 
-    func longestOnes(_ nums: [Int], _ k: Int) -> Int {
+    func longestSubarray(_ nums: [Int]) -> Int {
         slidingWindow = SlidingWindow(nums)
-        flipPossibleCount = k
         let inputNumsCount = nums.count
+        var zeroAdjustment = 1
 
         while slidingWindow.endIndex < inputNumsCount {
             if slidingWindowLastValue == 1 {
                 handleWhenValueIsOne()
             } else {
+                zeroAdjustment = 0
                 handleWhenValueIsZero()
             }
         }
 
-        maximumConsecutiveOnes = max(maximumConsecutiveOnes, currentWindowConsecutiveOnes)
+        maximumConsecutiveOnes = (max(maximumConsecutiveOnes, currentWindowConsecutiveOnes)) - zeroAdjustment
 
         return maximumConsecutiveOnes
     }
@@ -80,21 +85,21 @@ fileprivate class Solution {
 
     private func handleWhenValueIsZero() {
         // lets see if have chance to make 0 into 1 first using K(flipPossibleCount) value
-        if flipPossibleCount > 0 {
-            flipPossibleCount -= 1
-            currentWindowConsecutiveOnes += 1
+        if canIgnoreZero {
+            canIgnoreZero = false
             slidingWindow.endIndex += 1 // Slide window by one from end.
-        } else if flipPossibleCount == 0 {
+        } else {
             // We found maximum ConsecutiveOnes in this sliding-window
             maximumConsecutiveOnes = max(maximumConsecutiveOnes, currentWindowConsecutiveOnes)
 
-            // Adjust currentWindowConsecutiveOnes, before sliding next window
-            currentWindowConsecutiveOnes -= 1
+            while slidingWindowFirstValue != 0 {
+                // Adjust currentWindowConsecutiveOnes, before sliding next window
+                currentWindowConsecutiveOnes -= 1
 
-            if slidingWindowFirstValue == 0 {
-                flipPossibleCount += 1
+                slidingWindow.startIndex += 1 // Slide window by one from start.
             }
 
+            canIgnoreZero = true
             slidingWindow.startIndex += 1 // Slide window by one from start.
         }
     }
