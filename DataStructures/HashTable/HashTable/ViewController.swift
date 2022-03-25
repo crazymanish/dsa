cf83def9698a15554f92302ee8a84c97c682c9e5
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-03-25 21:58:36 +0100

    Day53: Number of Good Pairs - HashTable (#89)

---
+++ DataStructures/HashTable/HashTable/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HashTable
+//
+//  Created by Manish Rathi on 25/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        solve1()
+    }
+}
+
