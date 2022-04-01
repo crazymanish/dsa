b6e803c8152c157871a19a69064a6a8ad4308d92
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-04-01 09:53:12 +0200

    Day77: LeetCode-535. Encode and Decode TinyURL - HashTable

---
+++ DataStructures/HashTable/HashTable/ViewController+Challenge66.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController+Challenge66.swift
+//  HashTable
+//
+//  Created by Manish Rathi on 01/04/2022.
+//
+
+import Foundation
+/*
+ 535. Encode and Decode TinyURL
+ https://leetcode.com/problems/encode-and-decode-tinyurl/
+ Note: This is a companion problem to the System Design problem: Design TinyURL.
+ TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk. Design a class to encode a URL and decode a tiny URL.
+ There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+
+ Implement the Solution class:
+ Solution() Initializes the object of the system.
+ String encode(String longUrl) Returns a tiny URL for the given longUrl.
+ String decode(String shortUrl) Returns the original long URL for the given shortUrl. It is guaranteed that the given shortUrl was encoded by the same object.
+
+ Example 1:
+ Input: url = "https://leetcode.com/problems/design-tinyurl"
+ Output: "https://leetcode.com/problems/design-tinyurl"
+
+ Explanation:
+ Solution obj = new Solution();
+ string tiny = obj.encode(url); // returns the encoded tiny url.
+ string ans = obj.decode(tiny); // returns the original url after deconding it.
+ */
+
+extension ViewController {
+    func solve66() {
+        print("Setting up Challenge66 input!")
+
+        let input = "https://leetcode.com/problems/design-tinyurl"
+        print("Input: \(input)")
+        let obj = Codec()
+        let s = obj.encode(input)
+        let output = obj.decode(s)
+        print("Output: \(output)")
+    }
+}
+
+private class Codec {
+    var hashMap: [String: String] = [:]
+
+    // Encodes a URL to a shortened URL.
+    func encode(_ longUrl: String) -> String {
+        let uuid = UUID().uuidString
+        hashMap[uuid] = longUrl
+
+        return uuid
+    }
+
+    // Decodes a shortened URL to its original URL.
+    func decode(_ shortUrl: String) -> String {
+        return hashMap[shortUrl] ?? "404"
+    }
+}
