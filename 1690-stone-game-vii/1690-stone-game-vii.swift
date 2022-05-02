15c1fc3f5e1069d5c5e21b6621098063f90df1b8
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-02 22:44:18 +0200

    Time: 2838 ms (100.00%), Space: 22.2 MB (100.00%) - LeetHub

---
+++ 1690-stone-game-vii/1690-stone-game-vii.swift
@@ -0,0 +1,29 @@
+class Solution {
+    func stoneGameVII(_ stones: [Int]) -> Int {
+        func playStoneGame(_ startIndex: Int, _ endIndex: Int) -> Int {
+            if startIndex >= endIndex { return 0 }
+            
+            var totalValue = 0
+            for index in startIndex...endIndex { totalValue += stones[index] }
+            
+            let cacheValue = dpCache[startIndex][endIndex]
+            if cacheValue != -1 { return cacheValue }
+            
+            // Let's assume that Alice will pick startIndex
+            let possibleAnswer1 = (totalValue - stones[startIndex]) - playStoneGame(startIndex+1, endIndex)
+            
+            // Let's assume that Alice will pick endIndex
+            let possibleAnswer2 = (totalValue - stones[endIndex]) - playStoneGame(startIndex, endIndex-1)
+            
+            let answer = max(possibleAnswer1, possibleAnswer2)
+            
+            dpCache[startIndex][endIndex] = answer
+            return answer
+        }
+        
+        let stonesCount = stones.count
+        var dpCache = Array(repeating: Array(repeating: -1, count: stonesCount+1), count: stonesCount+1)
+        
+        return playStoneGame(0, (stonesCount-1))
+    }
+}
