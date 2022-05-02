a1c56bc5faa9865f0b6b777327a43415ed10e9a6
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-02 22:39:55 +0200

    Time: 1793 ms (11.11%), Space: 18.1 MB (44.44%) - LeetHub

---
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func minPairSum(_ nums: [Int]) -> Int {
+        let numsSorted = nums.sorted()
+        let numsCount = nums.count
+        
+        var leftPointer = 0
+        var rightPointer = numsCount-1
+        var maximumPairSum = Int.min
+        
+        while leftPointer < rightPointer {
+            let pairSum = numsSorted[leftPointer] + numsSorted[rightPointer]
+            maximumPairSum = max(maximumPairSum, pairSum)
+            
+            leftPointer += 1
+            rightPointer -= 1
+        }
+        
+        return maximumPairSum
+    }
+}
