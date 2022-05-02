35cf34fc3d7e7d8ba64e2716ffedc875d3eb3027
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-02 22:45:27 +0200

    Time: 186 ms (33.33%), Space: 22.7 MB (33.33%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.swift
@@ -0,0 +1,31 @@
+class Solution {
+    func winnerSquareGame(_ n: Int) -> Bool {
+        func playStoneGame(_ n: Int) -> Bool {
+            if n < 1 { return false }
+            
+            if let cacheValue = dpCache[n] { return cacheValue }
+            
+            var canIWin = false
+            
+            var currentSquareNumber = 1 
+            while currentSquareNumber*currentSquareNumber <= n {
+                let squareValue = currentSquareNumber*currentSquareNumber
+                let canNextPlayerWin = playStoneGame(n-squareValue)
+                
+                if canNextPlayerWin == false { 
+                    canIWin = true 
+                    break
+                }
+                
+                currentSquareNumber += 1
+            }
+            
+            dpCache[n] = canIWin
+            return canIWin
+        }
+        
+        var dpCache: [Bool?] = Array(repeating: nil, count: n+1)
+        
+        return playStoneGame(n)
+    }
+}
