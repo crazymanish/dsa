59b9301030b33ab824a65da8c763358655ada0e5
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-02 22:37:07 +0200

    Time: 23 ms (67.12%), Space: 14 MB (79.78%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.swift
@@ -0,0 +1,33 @@
+class Solution {
+    func findPeakElement(_ nums: [Int]) -> Int {
+        return peakIndexInMountainArray(nums)
+    }
+
+    // Ditto code: 852. Peak Index in a Mountain Array
+    // https://leetcode.com/problems/peak-index-in-a-mountain-array/
+    private func peakIndexInMountainArray(_ arr: [Int]) -> Int {
+        var startIndex = 0
+        var endIndex = arr.count-1
+        var midIndex = -1
+        
+        while startIndex <= endIndex {
+            midIndex = startIndex + (endIndex-startIndex) / 2
+            let midIndexValue = arr[midIndex]
+            
+            var leftValueOfMidIndex = Int.min
+            var rightValueOfMidIndex = Int.min
+            if midIndex-1 >= startIndex { leftValueOfMidIndex = arr[midIndex-1] }
+            if midIndex+1 <= endIndex { rightValueOfMidIndex = arr[midIndex+1] }
+            
+            if midIndexValue > leftValueOfMidIndex && midIndexValue > rightValueOfMidIndex {
+                break // we found the answer
+            } else if leftValueOfMidIndex > midIndexValue {
+                endIndex = midIndex-1 // Let's find answer in left-side
+            } else {
+                startIndex = midIndex+1 // Let's find answer in right-side
+            }
+        }
+        
+        return midIndex
+    }
+}
