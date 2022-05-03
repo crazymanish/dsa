73db951c2222b48222503ee99028d3cef12151ae
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-03 10:15:37 +0200

    Time: 141 ms (43.75%), Space: 13.5 MB (68.75%) - LeetHub

---
+++ 526-beautiful-arrangement/526-beautiful-arrangement.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func countArrangement(_ n: Int) -> Int {
+        var output = 0
+        var nums = Array(repeating: 0, count: n+1)
+        
+        func backtracking(_ currentNum: Int) {
+            if currentNum > n {
+                output += 1
+                return
+            }
+            
+            for index in 1...n {
+                if nums[index] == 0 && (currentNum%index == 0 || index%currentNum == 0) {
+                    nums[index] = currentNum
+                    backtracking(currentNum+1)
+                    nums[index] = 0 // Reset for next backtracking call
+                }
+            }
+        }
+        
+        backtracking(1)
+        return output
+    }
+}
