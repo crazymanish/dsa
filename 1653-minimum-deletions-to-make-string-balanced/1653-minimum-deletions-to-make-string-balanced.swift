45630c49e44eab962a2cb5a2e453df58d30d6dca
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-03 10:13:12 +0200

    Time: 225 ms (46.67%), Space: 14.5 MB (73.33%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func minimumDeletions(_ s: String) -> Int {
+        var prefixBCount = 0
+        var minimumDeletionsCount = 0
+        
+        for char in s {
+            if char == "b" {
+                prefixBCount += 1
+            } else if char == "a" && prefixBCount > 0 {
+                prefixBCount -= 1
+                minimumDeletionsCount += 1
+            }
+        }
+        
+        return minimumDeletionsCount
+    }
+}
