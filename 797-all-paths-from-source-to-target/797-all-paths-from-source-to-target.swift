213bd1d1dfec7e3538eb8bc3cacd6bd19f5ac329
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-03 21:46:59 +0200

    Time: 81 ms (71.54%), Space: 16 MB (59.23%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func allPathsSourceTarget(_ graph: [[Int]]) -> [[Int]] {
+        let sourceNode = 0
+        let destinationNode = graph.count-1
+        
+        var allPaths: [[Int]] = []
+        var currentPath: [Int] = []
+        
+        func findAllPathsUsingDFS(_ currentNode: Int) {
+            currentPath += [currentNode]
+            
+            if currentNode == destinationNode {
+                allPaths.append(currentPath)
+            }
+            
+            for nextNode in graph[currentNode] {
+                findAllPathsUsingDFS(nextNode)
+                
+                // backtracking, removing the last item for next recursion call
+                currentPath.removeLast() 
+            }
+        }
+        
+        findAllPathsUsingDFS(sourceNode)
+        return allPaths
+    }
+}
