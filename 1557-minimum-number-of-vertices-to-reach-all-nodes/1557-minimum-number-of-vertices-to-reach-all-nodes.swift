9703c66153f1fb76606299df3a0e93837d87cf67
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-04 21:36:26 +0200

    Time: 2711 ms (9.09%), Space: 24.8 MB (31.82%) - LeetHub

---
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func findSmallestSetOfVertices(_ n: Int, _ edges: [[Int]]) -> [Int] {
+        //Idea: We only have to count the number of nodes with zero incoming edges.
+        
+        var sourceNodes: Set<Int> = []
+        var destinationNodes: Set<Int> = []
+        
+        for edge in edges {
+            let sourceNode = edge[0]
+            let destinationNode = edge[1]
+            
+            destinationNodes.insert(destinationNode)
+            
+            if sourceNodes.contains(destinationNode) {
+                sourceNodes.remove(destinationNode)
+            }
+            
+            if destinationNodes.contains(sourceNode) == false {
+                sourceNodes.insert(sourceNode)
+            }
+        }
+        
+        return Array(sourceNodes)
+    }
+}
