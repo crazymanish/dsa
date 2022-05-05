e240d47489fa8d4f189ff92138911b551f6bac8c
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-05 22:28:12 +0200

    Time: 184 ms (41.28%), Space: 14 MB (92.44%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.swift
@@ -0,0 +1,29 @@
+class Solution {
+    func findCircleNum(_ isConnected: [[Int]]) -> Int {
+        let citiesCount = isConnected.count
+        var visitedCities = Array(repeating: false, count: citiesCount)
+        var numberOfProvinces = 0
+        
+        func visitCityUsingDFS(_ startingCityIndex: Int) {
+            visitedCities[startingCityIndex] = true
+            
+            for currentCityIndex in 0..<citiesCount {
+                if startingCityIndex == currentCityIndex { continue }
+                
+                let isBothCitiesConnected = isConnected[startingCityIndex][currentCityIndex] == 1
+                if isBothCitiesConnected && visitedCities[currentCityIndex] == false {
+                    visitCityUsingDFS(currentCityIndex)
+                }
+            }
+        }
+        
+        for startingCityIndex in 0..<citiesCount {
+            if visitedCities[startingCityIndex] == false {
+                visitCityUsingDFS(startingCityIndex)
+                numberOfProvinces += 1
+            }
+        }
+        
+        return numberOfProvinces
+    }
+}
