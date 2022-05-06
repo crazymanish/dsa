e8462e40ef593745baa279c86b4d82d91b966ba1
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-06 22:32:20 +0200

    Time: 36 ms (38.00%), Space: 14.4 MB (100.00%) - LeetHub

---
+++ 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.swift
@@ -0,0 +1,55 @@
+/**
+ * Definition for a Node.
+ * public class Node {
+ *     public var val: Int
+ *     public var children: [Node]
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.children = []
+ *     }
+ * }
+ */
+
+class Solution {
+    func maxDepth(_ root: Node?) -> Int {
+        guard let rootNode = root else { return 0 }
+        
+        let queue = Queue()
+        queue.enQueue(rootNode)
+        
+        var maxDepth = 0
+        
+        while queue.isEmpty == false {
+            let queueCount = queue.count
+            
+            for _ in 0..<queueCount {
+                let currentNode = queue.deQueue()!
+                
+                for childNode in currentNode.children {
+                    queue.enQueue(childNode)
+                }
+            }
+            
+            maxDepth += 1
+        }
+        
+        return maxDepth
+    }
+}
+
+class Queue {
+    private var array: [Node] = []
+    
+    var isEmpty: Bool { array.isEmpty }
+    var count: Int { array.count }
+    
+    func enQueue(_ element: Node) {
+        array.append(element)
+    }
+    
+    func deQueue() -> Node? {
+        if isEmpty { return nil }
+        
+        return array.removeFirst()
+    }
+}
