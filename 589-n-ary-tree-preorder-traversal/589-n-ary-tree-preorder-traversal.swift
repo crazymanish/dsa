dc32808f21e5f677267196fe42ad3af0de3d2e3d
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-07 21:21:51 +0200

    Time: 32 ms (71.00%), Space: 14.9 MB (74.00%) - LeetHub

---
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.swift
@@ -0,0 +1,26 @@
+/**
+ * Definition for a Node.
+ * public class Node {
+ *     public var val: Int
+ *     public var children: [Node]
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.children = []
+ *     }
+ * }
+ */
+
+class Solution {
+    func preorder(_ root: Node?) -> [Int] {
+    	guard let currentNode = root else { return [] }
+        
+        var childrenPreorder: [Int] = []
+        for childNode in currentNode.children {
+            let childNodePreorder = preorder(childNode)
+            
+            childrenPreorder += childNodePreorder
+        }
+        
+        return [currentNode.val] + childrenPreorder
+    }
+}
