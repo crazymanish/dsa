205d6328f711a51bd14c99e31947fec503d5a53d
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-09 21:05:50 +0200

    Time: 25 ms (42.86%), Space: 13.8 MB (98.44%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.swift
@@ -0,0 +1,36 @@
+class Solution {
+    func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
+        let lastIndex = candidates.count-1
+        
+        var allCombinations: [[Int]] = []
+        var currentCombination: [Int] = []
+        var currentCombinationSum = 0
+
+        func findCombinations(_ currentIndex: Int) {
+            if currentCombinationSum == target {
+                allCombinations.append(currentCombination)
+                return
+            }
+            
+            if currentCombinationSum > target { return }
+            if currentIndex > lastIndex { return }
+
+            // Choice1: Consider current index value for combination-sum
+            let value = candidates[currentIndex]
+                
+            currentCombination.append(value)
+            currentCombinationSum += value
+                
+            findCombinations(currentIndex)
+                
+            currentCombination.removeLast() // backtracking
+            currentCombinationSum -= value // backtracking
+                
+            // Choice2: Ignore current index value for combination-sum
+            findCombinations(currentIndex+1)
+        }
+
+        findCombinations(0)
+        return allCombinations
+    }
+}
