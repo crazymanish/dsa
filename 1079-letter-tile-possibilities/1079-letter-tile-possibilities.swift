fed5d879e34aa3566e52c4b5863f89e37c78ea10
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-15 20:40:09 +0200

    Time: 93 ms (8.33%), Space: 15.1 MB (25.00%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.swift
@@ -0,0 +1,35 @@
+class Solution {
+    func numTilePossibilities(_ tiles: String) -> Int {
+        let lastIndex = tiles.count-1
+        var tilesArray = Array(tiles)
+        var allPermutations: Set<String> = []
+        var currentPermutation: String = ""
+        
+        func findPermutations(_ currentIndex: Int) {
+            if currentIndex > lastIndex { return }
+            
+            for permutationIndex in currentIndex...lastIndex {
+                swap(currentIndex, permutationIndex)
+                
+                let currentIndexValue = String(tilesArray[currentIndex])
+                currentPermutation.append(currentIndexValue)
+                allPermutations.insert(currentPermutation)
+                
+                findPermutations(currentIndex+1)
+                
+                // backtracking
+                swap(currentIndex, permutationIndex)
+                currentPermutation.removeLast()
+            }
+        }
+        
+        func swap(_ firstIndex: Int, _ secondIndex: Int) {
+            let temp = tilesArray[firstIndex]
+            tilesArray[firstIndex] = tilesArray[secondIndex]
+            tilesArray[secondIndex] = temp
+        }
+        
+        findPermutations(0)
+        return allPermutations.count
+    }
+}
