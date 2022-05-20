0fd2d2ecc7a6c66ae55c6c97d539fc0238d8fb56
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-20 16:29:35 +0200

    Time: 136 ms (85.26%), Space: 15.9 MB (26.32%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.swift
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    var treeNodeValuesMap: [Int : Bool] = [:]
+    
+    func findTarget(_ root: TreeNode?, _ k: Int) -> Bool {
+        guard let root = root else { return false }
+        
+        let currentNodeValue = root.val
+        let remainingTarget = k - currentNodeValue
+        
+        if treeNodeValuesMap[remainingTarget] != nil { return true }
+        
+        treeNodeValuesMap[currentNodeValue] = true
+        
+        let findTargetInLeft = findTarget(root.left, k)
+        let findTargetInRight = findTarget(root.right, k)
+        
+        return findTargetInLeft || findTargetInRight
+    }
+}
