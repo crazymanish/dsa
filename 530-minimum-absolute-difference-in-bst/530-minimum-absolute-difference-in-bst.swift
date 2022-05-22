390ede5a3159db68e887c3db2aec7ef8df6c6bfd
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-22 09:38:21 +0200

    Time: 103 ms (19.44%), Space: 14.8 MB (58.33%) - LeetHub

---
+++ 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.swift
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    private var minimumDifference = Int.max
+    private var previousNode: TreeNode? = nil
+    
+    func getMinimumDifference(_ root: TreeNode?) -> Int {
+        findMinimumDifference(root)
+        
+        return minimumDifference
+    }
+    
+    private func findMinimumDifference(_ currentNode: TreeNode?) {
+        guard let currentNode = currentNode else { return }
+        
+        findMinimumDifference(currentNode.left)
+        
+        if previousNode != nil {
+            let currentDifference = abs(previousNode!.val - currentNode.val)
+            minimumDifference = min(minimumDifference, currentDifference)
+        }
+        
+        previousNode = currentNode
+        
+        findMinimumDifference(currentNode.right)
+    }
+}
