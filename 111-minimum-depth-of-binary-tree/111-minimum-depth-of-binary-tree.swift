a187af84bc2e92520ea93e2de13f5d1883ff2a59
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-05-27 12:59:32 +0200

    Time: 1242 ms (44.44%), Space: 24.9 MB (85.96%) - LeetHub

---
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.swift
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    private var minDepth = Int.max
+
+    func minDepth(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
+        
+        findAllDepths(root, 0)
+
+        return minDepth
+    }
+
+    private func findAllDepths(_ root: TreeNode?, _ currentDepth: Int) {
+        guard let currentNode = root else { return }
+
+        let newDepth = currentDepth + 1
+
+        if currentNode.left == nil && currentNode.right == nil {
+            minDepth = min(minDepth, newDepth)
+        }
+
+        findAllDepths(currentNode.left, newDepth)
+        findAllDepths(currentNode.right, newDepth)
+    }
+}
