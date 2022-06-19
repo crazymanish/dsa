4e03c9e938ef7e5e81a7c08db4e864cfe950eb5a
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-06-19 09:12:13 +0200

    Time: 15 ms (6.20%), Space: 13.6 MB (42.56%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.swift
@@ -0,0 +1,9 @@
+class Solution {
+    func isPowerOfTwo(_ n: Int) -> Bool {
+        if n == 1 { return true } // i.e 2° = 1
+        if n <= 0 { return false } // i.e 0 or negative value
+        if n % 2 == 1 { return false } // i.e 3/2 scenarios
+        
+        return isPowerOfTwo(n/2)
+    }
+}
