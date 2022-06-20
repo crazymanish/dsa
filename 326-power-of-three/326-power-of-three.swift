0ad7d582a8052bd61ddaf9ee61a3a1efe26918da
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-06-20 18:42:01 +0200

    Time: 97 ms (21.30%), Space: 14.2 MB (60.19%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.swift
@@ -0,0 +1,10 @@
+class Solution {
+    func isPowerOfThree(_ n: Int) -> Bool {
+        if n == 1 { return true } // i.e 3° = 1
+        if n <= 0 { return false } // i.e 0 or negative value
+        if n % 3 == 1 { return false } // i.e 4/3 scenarios
+        if n % 3 == 2 { return false } // i.e 5/3 scenarios
+
+        return isPowerOfThree(n/3)
+    }
+}
