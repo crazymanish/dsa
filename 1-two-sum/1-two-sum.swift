73a0eb81c5b3437d3a0a29395c8318371b84fb3e
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-06-23 10:26:39 +0200

    Time: 59 ms (68.75%), Space: 14.2 MB (80.08%) - LeetHub

---
+++ 1-two-sum/1-two-sum.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var hashMap: [Int : Int] = [:]
+        var output: [Int] = []
+        
+        var index = 0
+        for num in nums {
+            if let remainingTargetIndex = hashMap[target-num] {
+                output.append(remainingTargetIndex)
+                output.append(index)
+                break
+            }
+            
+            hashMap[num] = index
+            index += 1
+        }
+        
+        return output
+    }
+}
