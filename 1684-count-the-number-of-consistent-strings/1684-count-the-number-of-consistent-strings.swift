c0ee901ee5e49d69e900fb9c496df9294153fb6d
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-06-25 18:50:22 +0200

    Time: 329 ms (34.38%), Space: 14.9 MB (43.75%) - LeetHub

---
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func countConsistentStrings(_ allowed: String, _ words: [String]) -> Int {
+        typealias CharIsExistMap = [Character : Bool]
+        var hashMap: CharIsExistMap = [:]
+
+        for char in allowed {
+            hashMap[char] = true
+        }
+
+        var output = 0
+        for word in words {
+            var isInConsistentCharFound = false
+            for char in word {
+                if hashMap[char] == nil { 
+                    isInConsistentCharFound = true
+                    break 
+                }
+            }
+            
+            if isInConsistentCharFound == false { output += 1 }
+        }
+
+        return output
+    }
+}
