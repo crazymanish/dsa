35e30a8170bc98339013d7bb58a7761543bbabd0
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-07-01 07:55:27 +0200

    Time: 11 ms (66.18%), Space: 14.2 MB (45.59%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.swift
@@ -0,0 +1,28 @@
+class Solution {
+    func uniqueOccurrences(_ arr: [Int]) -> Bool {
+        var hashMap: [Int : Int] = [:]
+        
+        for number in arr {
+            if let hashMapOccurrence = hashMap[number] {
+                hashMap[number] = hashMapOccurrence + 1
+            } else {
+                hashMap[number] = 1
+            }
+        }
+        
+        var isAllUniqueOccurrences = true
+
+        var uniqueHashMap: [Int : Bool] = [:]
+        for (_, value) in hashMap {
+            if uniqueHashMap[value] == nil {
+                uniqueHashMap[value] = true
+            } else {
+                isAllUniqueOccurrences = false
+                break
+            }
+        }
+        
+        
+        return isAllUniqueOccurrences
+    }
+}
