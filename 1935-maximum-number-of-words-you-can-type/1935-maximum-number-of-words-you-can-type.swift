70c6b1ea457f6bae3bc0568fdbccb8bcf4607f57
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-07-02 11:33:48 +0200

    Time: 10 ms (75.00%), Space: 15.3 MB (8.33%) - LeetHub

---
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func canBeTypedWords(_ text: String, _ brokenLetters: String) -> Int {
+        var brokenCharHashMap: [Character : Bool] = [:]
+        for char in brokenLetters { brokenCharHashMap[char] = true }
+        
+        var canBeTypedWords = 0
+        
+        let words = text.components(separatedBy: " ")
+        for word in words {
+            var canTypeCurrentWord = true
+            for char in word {
+                if brokenCharHashMap[char] != nil {
+                    canTypeCurrentWord = false
+                    break
+                }
+            }
+            
+            if canTypeCurrentWord { canBeTypedWords += 1 }
+            
+        }
+        
+        return canBeTypedWords
+    }
+}
