67a042ffcef90b2741232b56ea87edfb9e9ced13
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-07-08 17:50:57 +0200

    Time: 11 ms (48.15%), Space: 14.5 MB (25.93%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.swift
@@ -0,0 +1,28 @@
+class Solution {
+    func maxNumberOfBalloons(_ text: String) -> Int {
+        var hashMap: [Character : Int] = [:]
+        
+        for char in text {
+            let hashMapValue = hashMap[char] ?? 0
+            hashMap[char] = hashMapValue + 1
+        }
+        
+        var maxNumber = Int.max
+        
+        if let b = hashMap["b"],
+        let a = hashMap["a"],
+        let l = hashMap["l"],
+        let o = hashMap["o"],
+        let n = hashMap["n"] {
+            maxNumber = min(maxNumber, b)
+            maxNumber = min(maxNumber, a)
+            maxNumber = min(maxNumber, l/2)
+            maxNumber = min(maxNumber, o/2)
+            maxNumber = min(maxNumber, n)   
+        } else {
+            maxNumber = 0
+        }
+        
+        return maxNumber
+    }
+}
