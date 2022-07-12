17c17ba8801479f2b475ffbffad12c31733ed754
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-07-12 16:35:00 +0200

    Time: 49 ms (86.67%), Space: 14.8 MB (20.00%) - LeetHub

---
+++ 748-shortest-completing-word/748-shortest-completing-word.swift
@@ -0,0 +1,45 @@
+class Solution {
+    func shortestCompletingWord(_ licensePlate: String, _ words: [String]) -> String {
+        var hashMap: [Character : Int] = [:]
+        
+        for scalar in licensePlate.lowercased().unicodeScalars {
+            let value = scalar.value
+            
+            if (value >= 97 && value <= 122) { // to find a-z
+                let char = Character(scalar)
+                let hashMapValue = hashMap[char] ?? 0
+                hashMap[char] = hashMapValue + 1
+            }
+        }
+        
+        var shortestWord: String? = nil
+        
+        for word in words {
+            var licensePlateHashMap = hashMap
+            
+            for char in word {
+                if let hashMapValue = licensePlateHashMap[char] {
+                    licensePlateHashMap[char] = hashMapValue - 1
+                }
+            }
+            
+            var canCompleteWord = true
+            for (_, value) in licensePlateHashMap {
+                if value > 0 {
+                    canCompleteWord = false
+                    break
+                }
+            }
+            
+            if canCompleteWord {
+                if let previousShortestWord = shortestWord {
+                    if word.count < previousShortestWord.count { shortestWord = word }
+                } else {
+                    shortestWord = word
+                }
+            }
+        }
+        
+        return shortestWord!
+    }
+}
