8e88b084a71cd780c58349545ae7b487579defad
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-07-12 16:33:53 +0200

    Time: 379 ms (86.71%), Space: 16.7 MB (27.75%) - LeetHub

---
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func findDisappearedNumbers(_ numbers: [Int]) -> [Int] {
+        var hashMap: [Int : Bool] = [:]
+        for number in numbers { hashMap[number] = true }
+
+        var disappearedNumbers: [Int] = []
+        
+        for number in 1...numbers.count {
+            if hashMap[number] == nil { disappearedNumbers.append(number) }
+        }
+        
+        return disappearedNumbers
+    }
+}
