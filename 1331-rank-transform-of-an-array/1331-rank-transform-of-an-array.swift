7d9c842200e891cb23b68c0acf66ec539fe6c53c
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-07-13 09:53:53 +0200

    Time: 580 ms (42.86%), Space: 23.1 MB (42.86%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func arrayRankTransform(_ array: [Int]) -> [Int] {
+        let sortedArray = array.sorted()
+        
+        var hashMap: [Int : Int] = [:]
+        
+        var currentRank = 1
+        for element in sortedArray {
+            if hashMap[element] == nil {
+                hashMap[element] = currentRank
+                currentRank += 1
+            }
+        }
+        
+        var outputArray: [Int] = []
+        for element in array {
+            outputArray.append(hashMap[element]!)
+        }
+        
+        return outputArray
+    }
+}
