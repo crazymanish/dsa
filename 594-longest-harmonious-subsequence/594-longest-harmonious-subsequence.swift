19c1cfd1a13166bbb0558ab482cdf6a8b4fcaa40
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-07-17 10:54:02 +0200

    Time: 517 ms (19.05%), Space: 14.9 MB (19.05%) - LeetHub

---
+++ 594-longest-harmonious-subsequence/594-longest-harmonious-subsequence.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func findLHS(_ nums: [Int]) -> Int {
+        var hashMap: [Int : Int] = [:]
+        
+        for num in nums {
+            let hashMapValue = hashMap[num] ?? 0
+            hashMap[num] = hashMapValue + 1
+        }
+        
+        var outputResult = 0
+        
+        for (key, value) in hashMap {
+            if let plusOneValue = hashMap[key+1] {
+                let result = value + plusOneValue
+                outputResult = max(outputResult, result)
+            }
+        }
+        
+        return outputResult
+    }
+}
