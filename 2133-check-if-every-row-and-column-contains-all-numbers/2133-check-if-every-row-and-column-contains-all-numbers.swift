e6fbc8ffbd1e1ab9ca02a21d4e04f562b7f983e9
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-07-18 08:42:13 +0200

    Time: 1714 ms (6.25%), Space: 14 MB (96.88%) - LeetHub

---
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.swift
@@ -0,0 +1,28 @@
+class Solution {
+    func checkValid(_ matrix: [[Int]]) -> Bool {
+        let n = matrix.count
+        var isValid = true
+        
+        for i in 0..<n {
+            var rowHashMap: [Int : Bool] = [:]
+            var columnHashMap: [Int : Bool] = [:]
+            
+            for j in 0..<n {
+                 rowHashMap[matrix[i][j]] = true
+                 columnHashMap[matrix[j][i]] = true
+            }
+            
+            for key in 1...n {
+                if rowHashMap[key] == nil || columnHashMap[key] == nil {
+                    isValid = false
+                    break
+                }
+            }
+            
+            if isValid == false { break }
+            
+        }
+        
+        return isValid
+    }
+}
