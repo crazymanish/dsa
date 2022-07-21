cae36eae71596afceabc45854922b2cf3f3d5995
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-07-21 17:52:17 +0200

    Time: 88 ms (8.33%), Space: 16.7 MB (11.11%) - LeetHub

---
+++ 819-most-common-word/819-most-common-word.swift
@@ -0,0 +1,34 @@
+class Solution {
+    func mostCommonWord(_ paragraph: String, _ banned: [String]) -> String {
+        var text = paragraph.lowercased()
+        text = text.replacingOccurrences(
+            of: "[!?',;.]", 
+            with: " ", 
+            options: [.regularExpression])
+        let words = text.components(separatedBy: " ")
+        
+        var hashMap: [String : Int] = [:]
+        for word in words {
+            let hashMapValue = hashMap[word] ?? 0
+            hashMap[word] = hashMapValue + 1
+        }
+        
+        hashMap[""] = -1
+        
+        for word in banned {
+            hashMap[word] = -1
+        }
+        
+        var mostCommonWord = ""
+        var mostCommonWordTimes = Int.min
+        
+        for (key, value) in hashMap {
+            if value > mostCommonWordTimes {
+                mostCommonWordTimes = value
+                mostCommonWord = key
+            }
+        }
+        
+        return mostCommonWord
+    }
+}
