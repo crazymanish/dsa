e3975dac93c81fb2b9e6c3807a7084498a3267f5
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-07-22 09:28:21 +0200

    Time: 23 ms (37.26%), Space: 15.1 MB (36.94%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func isIsomorphic(_ s: String, _ t: String) -> Bool {
+        guard s.count == t.count else { return false }
+        
+        var sHashMap: [Character:Character] = [:]
+        var tHashMap: [Character:Character] = [:]
+        
+        let sArray = Array(s)
+        let tArray = Array(t)
+        
+        for (index, sChar) in sArray.enumerated() {
+            let tChar = tArray[index]
+            
+            if sHashMap[sChar] == nil && tHashMap[tChar] == nil {
+                sHashMap[sChar] = tChar
+                tHashMap[tChar] = sChar
+            } else if (sHashMap[sChar] != tChar) || (tHashMap[tChar] != sChar) {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
