8622e7f220adcdbc39d979456a4c84dd9c89931b
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-07-25 08:01:20 +0200

    Time: 47 ms (58.33%), Space: 14.2 MB (87.50%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func commonChars(_ words: [String]) -> [String] {
+        if words.isEmpty { return [] }
+
+        var result = words[0]
+        
+        for word in words {
+            var currentWord = word
+            var newResult = ""
+            
+            for char in result {
+                if let index = currentWord.firstIndex(of: char) {
+                    newResult.append(char)
+                    currentWord.remove(at: index)
+                }
+            }
+            
+            result = newResult
+        }
+        
+        return result.map { String($0) }
+    }
+}
