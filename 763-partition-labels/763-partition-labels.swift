c71fa283c1d291ac17c6598a11925ce8e73d32b2
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-07-27 20:32:43 +0200

    Time: 3 ms (100.00%), Space: 14.6 MB (48.28%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.swift
@@ -0,0 +1,33 @@
+class Solution {
+    func partitionLabels(_ s: String) -> [Int] {
+        typealias LastIndex = Int
+        var hashMap: [Character : LastIndex] = [:]
+        
+        var index = 0
+        for char in s {
+            hashMap[char] = index
+            
+            index += 1
+        }
+        
+        var output: [Int] = []
+        
+        var currentPartitionLength = 0
+        var currentPartitionEndIndex = 0
+        var currentIndex = 0
+        for char in s {
+            let currentCharLastIndex = hashMap[char]!
+            currentPartitionEndIndex = max(currentPartitionEndIndex, currentCharLastIndex)
+            currentPartitionLength += 1
+            
+            if currentIndex == currentPartitionEndIndex {
+                output.append(currentPartitionLength)
+                currentPartitionLength = 0
+            }
+            
+            currentIndex += 1
+        }
+        
+        return output
+    }
+}
