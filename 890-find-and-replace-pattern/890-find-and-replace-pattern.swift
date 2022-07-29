b843a9a45be082e9c7383fb818b4840d5aa0f159
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-07-29 16:00:43 +0200

    Time: 18 ms (10.00%), Space: 14 MB (100.00%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.swift
@@ -0,0 +1,36 @@
+class Solution {
+    func findAndReplacePattern(_ words: [String], _ pattern: String) -> [String] {
+        var output: [String] = []
+        
+        for word in words {
+            if isIsomorphic(word, pattern) {
+                output.append(word)
+            }
+        }
+        
+        return output
+    }
+
+    private func isIsomorphic(_ s: String, _ t: String) -> Bool {
+        guard s.count == t.count else { return false }
+
+        var sHashMap: [Character:Character] = [:]
+        var tHashMap: [Character:Character] = [:]
+
+        let sArray = Array(s)
+        let tArray = Array(t)
+
+        for (index, sChar) in sArray.enumerated() {
+            let tChar = tArray[index]
+
+            if sHashMap[sChar] == nil && tHashMap[tChar] == nil {
+                sHashMap[sChar] = tChar
+                tHashMap[tChar] = sChar
+            } else if (sHashMap[sChar] != tChar) || (tHashMap[tChar] != sChar) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
