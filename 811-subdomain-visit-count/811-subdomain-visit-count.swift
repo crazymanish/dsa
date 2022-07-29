aba0777532b9c0ad2627255c6a235c6dec466353
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-07-29 16:01:25 +0200

    Time: 156 ms (11.90%), Space: 15.7 MB (7.14%) - LeetHub

---
+++ 811-subdomain-visit-count/811-subdomain-visit-count.swift
@@ -0,0 +1,35 @@
+class Solution {
+    func subdomainVisits(_ cpdomains: [String]) -> [String] {
+        var hashMap: [String : Int] = [:]
+        
+        for cpdomain in cpdomains {
+            let cpdomainComponents = cpdomain.components(separatedBy: " ")
+            let visitedTimes = Int(cpdomainComponents[0])!
+            let domain = cpdomainComponents[1]
+            let subdomains = domain.components(separatedBy: ".")
+            let subdomainsCount = subdomains.count
+            
+            for index in 0..<subdomainsCount {
+                var currentIndex = index
+                var subdomain = ""
+                
+                while currentIndex < subdomainsCount {
+                    subdomain += "\(subdomains[currentIndex])."
+                    currentIndex += 1
+                }
+                
+                subdomain.removeLast() // extra dot
+                
+                let hashMapValue = hashMap[subdomain] ?? 0
+                hashMap[subdomain] = hashMapValue + visitedTimes
+            }
+        }
+        
+        var output: [String] = []
+        for (key, value) in hashMap {
+            output.append("\(value) \(key)")
+        }
+        
+        return output
+    }
+}
