6f50da39b77fd0479afa33b412c12a871c84d2b2
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-08-02 11:48:36 +0200

    Update 378-kth-smallest-element-in-a-sorted-matrix.swift

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.swift
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.swift
@@ -7,3 +7,34 @@ class Solution {
         return inputArray[k-1]
     }
 }
+
+
+class Solution {
+    func kthSmallest(_ matrix: [[Int]], _ k: Int) -> Int {
+        let n = matrix.count
+        
+        var minElement = matrix[0][0]
+        var maxElement = matrix[n-1][n-1]
+        
+        while minElement != maxElement {
+            let midElement = minElement + (maxElement - minElement) / 2
+            var minElementsThanMidElement = 0
+            
+            for row in 0..<n {
+                for column in 0..<n {
+                    if matrix[row][column] > midElement { break }
+                    
+                    minElementsThanMidElement += 1
+                }
+            }
+            
+            if minElementsThanMidElement < k {
+                minElement = midElement + 1
+            } else {
+                maxElement = midElement
+            }
+        }
+        
+        return minElement
+    }
+}
