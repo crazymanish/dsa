ceb3e2f3503b38686a448f7df447fbdb56940c5e
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-08-17 09:36:56 +0200

    leetcode-804 https://leetcode.com/problems/unique-morse-code-words/

--- 804-unique-morse-code-words/804-unique-morse-code-words.swift
+++ 804-unique-morse-code-words/804-unique-morse-code-words.swift
@@ -30,3 +30,35 @@ class Solution {
         return hashMap
     }
 }
+
+class Solution2UsingSet {
+    func uniqueMorseRepresentations(_ words: [String]) -> Int {
+        let morseCodeHashMap = getMorseCodeHashMap()
+        var transformationSet = Set<String>()
+        var uniqueTransformationCount = 0
+        
+        for word in words {
+            var transformationString = ""
+            for character in word {
+                transformationString += morseCodeHashMap[character]!
+            }
+            
+            if transformationSet.insert(transformationString).inserted {
+                uniqueTransformationCount += 1
+            }
+        }
+        
+        return uniqueTransformationCount
+    }
+    
+    private func getMorseCodeHashMap() -> [Character : String] {
+        var hashMap: [Character : String] = [:]
+        
+        let codes = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        let alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        
+        for index in 0..<26 { hashMap[Character(alphabets[index])] = codes[index] }
+        
+        return hashMap
+    }
+}
