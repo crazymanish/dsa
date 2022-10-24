dce9d8be42e12bbc4f33c2de41ccf91ec88e2ca5
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-10-24 09:50:20 +0200

    Time: 217 ms (33.33%), Space: 35 MB (8.33%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func maxLength(_ arr: [String]) -> Int {
+        var dp = [Set<Character>()]
+        for s in arr {
+            if Set(s).count < s.count {continue}
+            let set = Set(s)
+            for c in dp {
+                if !set.isDisjoint(with: c) {continue}
+                dp.append(set.union(c))
+            }
+        }
+        
+        return dp.map { $0.count }.max()!
+    }
+}
