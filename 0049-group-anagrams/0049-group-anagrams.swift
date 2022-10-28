87eed1b99e31934e9ecd79b3e2e209174da0f979
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-10-28 09:01:42 +0200

    Time: 139 ms (83.03%), Space: 16.4 MB (40.84%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        if strs.count == 1 { return [strs] }
+        
+        var hashMap: [String : [String]] = [:]
+        
+        for str in strs {
+            let sortedStr = String(str.sorted())
+            
+            let hashMapValue = hashMap[sortedStr] ?? []
+            hashMap[sortedStr] = hashMapValue + [str]
+        }
+        
+        return Array(hashMap.values)
+    }
+}
