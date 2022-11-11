3458c11e260fee0e9eb1710bd88f15fcecc26b61
Manish Rathi <i.am.manish.rathi@gmail.com>
2022-11-11 18:58:44 +0100

    Time: 136 ms (36.67%), Space: 15.1 MB (10.30%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+        // Just shift non-duplicate elements to left-most
+        
+        var leftIndexToShift = 0 
+        for index in 1..<nums.count {
+            let previousIndexValue = nums[index-1]
+            let currentIndexValue = nums[index]
+            
+            if currentIndexValue == previousIndexValue {
+               // noop: ignore left-shift, 
+               // this is duplicate element
+            } else {
+                nums[leftIndexToShift+1] = currentIndexValue
+                
+                leftIndexToShift += 1
+            }
+        }
+        
+        return leftIndexToShift + 1
+    }
+}
