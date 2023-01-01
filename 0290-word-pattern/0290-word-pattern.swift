14c9fb5e6da77656bcd8f516c13d937d83af424c
Manish Rathi <i.am.manish.rathi@gmail.com>
2023-01-01 08:52:32 +0100

    Time: 6 ms (37.50%), Space: 15.2 MB (7.95%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func wordPattern(_ pattern: String, _ s: String) -> Bool {
+        let patternArray = Array(pattern)
+        let sArray = s.components(separatedBy: " ")
+        
+        guard patternArray.count == sArray.count else { return false }
+        
+        var patternHashMap: [Character: String] = [:]
+        var sHashMap: [String: Character] = [:]
+        
+        for (index, patternChar) in patternArray.enumerated() {
+            let sString = sArray[index]
+
+            if patternHashMap[patternChar] == nil && sHashMap[sString] == nil {
+                patternHashMap[patternChar] = sString
+                sHashMap[sString] = patternChar
+            } else if (patternHashMap[patternChar] != sString) || (sHashMap[sString] != patternChar) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
