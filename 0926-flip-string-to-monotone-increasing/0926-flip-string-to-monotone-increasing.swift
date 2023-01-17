c86104bf4275c93eba7fd3f3a43dd3a1a1568fb6
Manish Rathi <i.am.manish.rathi@gmail.com>
2023-01-17 06:30:41 +0100

    Time: 35 ms (50.00%), Space: 14.1 MB (100.00%) - LeetHub

---
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.swift
@@ -0,0 +1,19 @@
+class Solution {
+    // Ditto question: 1653. Minimum Deletions to Make String Balanced
+    // https://leetcode.com/problems/minimum-deletions-to-make-string-balanced/
+    func minFlipsMonoIncr(_ s: String) -> Int {
+        var prefix1Count = 0
+        var minimumDeletionsCount = 0
+        
+        for char in s {
+            if char == "1" {
+                prefix1Count += 1
+            } else if char == "0" && prefix1Count > 0 {
+                prefix1Count -= 1
+                minimumDeletionsCount += 1
+            }
+        }
+        
+        return minimumDeletionsCount
+    }
+}
