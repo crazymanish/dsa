8f33a32f82589abc5db6a0d7c8f8068e80f56aec
Manish Rathi <i.am.manish.rathi@gmail.com>
2023-02-04 11:23:09 +0100

    Time: 12 ms (92.41%), Space: 14.5 MB (62.76%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.swift
@@ -0,0 +1,35 @@
+class Solution {
+    func checkInclusion(_ s1: String, _ s2: String) -> Bool {
+        let pattern = Array(s1)
+        let targetStr = Array(s2)
+
+        func getIndex(_ char: Character) -> Int {
+            Int(char.asciiValue! - Character("a").asciiValue!)
+        }
+        
+        var patternFreqTable = Array(repeating: 0, count: 26)
+        pattern.forEach { char in
+            let index = getIndex(char)
+            patternFreqTable[index] += 1
+        }
+
+        var start = 0
+        var targetFreqTable = Array(repeating: 0, count: 26)
+        for (end, char) in targetStr.enumerated() {
+            let index = getIndex(char)
+            targetFreqTable[index] += 1
+        
+            if end >= pattern.count {
+                let indexToDelete = getIndex(targetStr[start])
+                start += 1
+                targetFreqTable[indexToDelete] -= 1
+            }
+        
+            if targetFreqTable.elementsEqual(patternFreqTable) {
+                return true
+            }
+        }
+    
+        return false
+    }
+}
