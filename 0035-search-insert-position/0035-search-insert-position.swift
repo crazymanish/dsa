27f304b03a239a9d7fcc0295717620b7c6e807ec
Manish Rathi <i.am.manish.rathi@gmail.com>
2023-02-20 07:36:32 +0100

    Time: 26 ms (64.12%), Space: 14.4 MB (45.09%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.swift
@@ -0,0 +1,31 @@
+class Solution {
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        let lastIndex = nums.count-1
+        
+        func findInsertIndexUsingRecursiveBinarySearch(_ startIndex: Int, _ endIndex: Int) -> Int {
+            if startIndex > endIndex { return -1 } // Not-found exit base-case
+            
+            let midIndex = startIndex + (endIndex-startIndex) / 2
+            let midIndexValue = nums[midIndex]
+            
+            // Found answer that is equal to target
+            if midIndexValue == target { return midIndex }
+            
+            // Find left,right value of midIndex
+            let leftValue = midIndex-1 >= 0 ? nums[midIndex-1] : Int.min
+            let rightValue = midIndex+1 <= lastIndex ? nums[midIndex+1] : Int.max
+            
+            // check left-right answer
+            if midIndexValue > target && leftValue < target { return midIndex }
+            if midIndexValue < target && rightValue > target { return midIndex+1 }
+            
+            if midIndexValue > target {
+                return findInsertIndexUsingRecursiveBinarySearch(startIndex, midIndex-1) // Keep finding left side
+            } else {
+                return findInsertIndexUsingRecursiveBinarySearch(midIndex+1, endIndex) // Keep finding right side
+            }
+        }
+        
+        return findInsertIndexUsingRecursiveBinarySearch(0, lastIndex)
+    }
+}
