57116fa9fab86b738bd759534e19e7d77fd58f7c
Manish Rathi <i.am.manish.rathi@gmail.com>
2023-03-11 07:31:29 +0100

    Time: 40 ms (71.43%), Space: 17.9 MB (53.57%) - LeetHub

---
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.swift
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func sortedListToBST(_ head: ListNode?) -> TreeNode? {
+        var nodes = [TreeNode?]()
+        var nodeCount = 0
+        var currentNode = head
+            
+        while(currentNode != nil) {
+            nodes.append(TreeNode(currentNode!.val))
+            nodeCount += 1
+            
+            currentNode = currentNode?.next
+        }
+        
+        func build(_ leftIndex: Int, _ rightIndex: Int) -> TreeNode? {
+            if(leftIndex > rightIndex) { return nil }
+            if(leftIndex == rightIndex) { return nodes[leftIndex] }
+        
+            let mid = leftIndex + ((rightIndex - leftIndex) / 2)
+        
+            var rootNode = nodes[mid]
+            rootNode?.left = build(leftIndex, mid - 1)
+            rootNode?.right = build(mid + 1, rightIndex)
+            return rootNode
+        }
+        
+        return build(0, nodes.count - 1)
+    }
+}
