e1073ca03fa54df02b082dc4703cb5bc32944922
Manish Rathi <i.am.manish.rathi@gmail.com>
2023-03-27 08:43:50 +0200

    Time: 48 ms (85.25%), Space: 14.1 MB (98.36%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func minPathSum(_ grid: [[Int]]) -> Int {
+        var matrix = grid
+        let rows = matrix.count
+        let columns = matrix[0].count
+        
+        for row in (0..<rows).reversed() {
+            for column in (0..<columns).reversed() {
+                let isLastRow = row == rows-1
+                let isLastColumn = column == columns-1
+                
+                if isLastRow && isLastColumn { continue }
+                
+                let downValue = isLastRow ? Int.max : matrix[row+1][column]
+                let rightValue = isLastColumn ? Int.max : matrix[row][column+1]
+                
+                matrix[row][column] += min(downValue, rightValue)
+            }    
+        }
+        
+        return matrix[0][0]
+    }
+}
