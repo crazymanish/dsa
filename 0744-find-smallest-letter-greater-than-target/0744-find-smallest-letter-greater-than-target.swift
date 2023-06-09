57b916c765852e3e51aa77c755b072ff294918b5
Manish Rathi <i.am.manish.rathi@gmail.com>
2023-06-09 06:43:10 +0200

    Time: 77 ms (97.30%), Space: 14.6 MB (67.57%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.swift
@@ -0,0 +1,25 @@
+class Solution {
+    ///
+    /// The key part of this binary search is:
+    /// 1. left will be the first possible valid choice.
+    /// 2. right - left >= 1, then stop when left == right
+    /// 3. In the case left move to all the way to the end, but right never changed, we didn't find the valid choice.
+    ///
+    func nextGreatestLetter(_ letters: [Character], _ target: Character) -> Character {
+        var left = 0
+        var right = letters.count
+        
+        while left < right {
+            let mid = left + (right - left) / 2
+            
+            if letters[mid] <= target {
+                left = mid + 1
+            } else {
+                right = mid
+            }
+        }
+        
+        // print("Index: \(left)")
+        return letters[left % letters.count]
+    }
+}
