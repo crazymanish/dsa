28f9cc6b0b3bbd7b298854c229b083a27e0f82b0
Manish Rathi <i.am.manish.rathi@gmail.com>
2023-06-24 11:06:10 +0200

    Time: 320 ms (25.00%), Space: 14.9 MB (25.00%) - LeetHub

---
+++ 0956-tallest-billboard/0956-tallest-billboard.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func tallestBillboard(_ rods: [Int]) -> Int {
+        var dp = [0: 0]
+        
+        for i in rods {
+            var cur = [Int: Int]()
+            for (sum, height) in dp {
+                cur[sum+i] = max(dp[sum]!+i, cur[sum+i, default: 0])
+                cur[sum] = max(dp[sum]!, cur[sum, default: 0])
+                cur[sum-i] = max(dp[sum]!, cur[sum-i, default: 0])
+            }
+            dp = cur
+        }
+        
+        return dp[0, default: 0]
+    }
+}
