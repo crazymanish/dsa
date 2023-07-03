409f86cc8dc1fbfb59667beba549b037a9c0cf1e
Manish Rathi <i.am.manish.rathi@gmail.com>
2023-07-03 04:23:21 +0200

    Time: 11 ms (20.00%), Space: 14.7 MB (50.00%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func buddyStrings(_ s1: String, _ s2: String) -> Bool {
+        guard s1.count == s2.count else { return false }
+        
+        if s1 == s2 { return Set(s1).count < s1.count }
+        
+        var diffs: [Int] = []
+        let array1 = Array(s1) 
+        let array2 = Array(s2)
+            
+        for index in 0..<array1.count {
+            if array1[index] != array2[index] {
+                diffs.append(index)
+            }
+        }
+            
+        guard diffs.count == 2 else { return false }
+            
+        let i = diffs[0] 
+        let j = diffs[1]
+            
+        return array1[i] == array2[j] && array1[j] == array2[i]
+    }
+}
