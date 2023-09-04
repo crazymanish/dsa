3d317e3f756ef9b2fb2f293f556a7ca22914bf58
Manish Rathi <i.am.manish.rathi@gmail.com>
2023-09-04 05:32:03 +0200

    Time: 48 ms (94.42%), Space: 15.6 MB (65.74%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.swift
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func hasCycle(_ head: ListNode?) -> Bool {
+        if head == nil { return false }
+        
+        var slowPointer = head
+        var fastPointer = head?.next
+        
+        var hasCycle = false
+        
+        while true {
+            if fastPointer == nil { break }
+            
+            if fastPointer === slowPointer {
+                hasCycle = true
+                break
+            }
+            
+            slowPointer = slowPointer?.next
+            fastPointer = fastPointer?.next?.next
+        }
+        
+        return hasCycle
+    }
+}
