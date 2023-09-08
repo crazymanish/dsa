34e41acbbba5b56bf5eaea9a8b3c9904325382ae
Manish Rathi <i.am.manish.rathi@gmail.com>
2023-09-08 07:08:58 +0200

    Time: 3 ms (92.21%), Space: 14.2 MB (8.23%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func generate(_ numRows: Int) -> [[Int]] {
+        var output = [[1]]
+        
+        for _ in 1..<numRows {
+            let previousRow = [0] + output.last! + [0]
+            
+            var currentRow: [Int] = []
+            for index in 1...output.last!.count+1 {
+                let currentValue = previousRow[index-1] + previousRow[index]
+                currentRow.append(currentValue)
+            }
+            
+            output.append(currentRow)
+        }
+        
+        return output
+    }
+}
