79ee1c290a3a306c791fa02201b6dcfc2458efe1
Manish Rathi <i.am.manish.rathi@gmail.com>
2023-10-20 12:10:14 +0200

    Time: 29 ms (41.46%), Space: 16.3 MB (41.46%) - LeetHub

---
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.swift
@@ -0,0 +1,52 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public func isInteger() -> Bool
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     public func getInteger() -> Int
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public func setInteger(value: Int)
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public func add(elem: NestedInteger)
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     public func getList() -> [NestedInteger]
+ * }
+ */
+
+class NestedIterator {
+    private var result: [Int]
+
+    init(_ nestedList: [NestedInteger]) {
+        result = []
+        go(nestedList)
+    }
+
+    func go(_ list: [NestedInteger]) {
+        for value in list {
+            if value.isInteger() {
+                result.append(value.getInteger())
+            } else {
+                go(value.getList())
+            }
+        }
+    }
+    
+    func next() -> Int { result.removeFirst() }
+    
+    func hasNext() -> Bool { !result.isEmpty }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * let obj = NestedIterator(nestedList)
+ * let ret_1: Int = obj.next()
+ * let ret_2: Bool = obj.hasNext()
+ */
