9a7c0018484188f1513e567192ef45a67b768a8f
Manish Rathi <i.am.manish.rathi@gmail.com>
2024-03-02 13:28:22 +0100

    Time: 163 ms (60.31%), Space: 17.7 MB (9.14%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.swift
@@ -0,0 +1,28 @@
+class Solution {
+    // Follow up: Squaring each element and sorting the new array is very trivial, 
+    // could you find an O(n) solution using a different approach?
+    //
+    // Yes, using two pointers.
+    func sortedSquares(_ nums: [Int]) -> [Int] {
+        var output: [Int] = []
+        var leftPointer = 0
+        var rightPointer = nums.count-1
+        
+        while leftPointer <= rightPointer {
+            let leftSquareRoot = nums[leftPointer] * nums[leftPointer]
+            let rightSquareRoot = nums[rightPointer] * nums[rightPointer]
+            
+            if leftSquareRoot > rightSquareRoot {
+                output.insert(leftSquareRoot, at: 0)
+                
+                leftPointer += 1
+            } else {
+                output.insert(rightSquareRoot, at: 0)
+                
+                rightPointer -= 1
+            }
+        }
+        
+        return output
+    }
+}
