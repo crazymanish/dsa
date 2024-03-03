84279acd666fa43061be1bade4bc1c8621fe7c16
Manish Rathi <i.am.manish.rathi@gmail.com>
2024-03-03 10:08:19 +0100

    Time: 3 ms (86.47%), Space: 15.4 MB (65.75%) - LeetHub

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.swift
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        if head == nil || head?.next == nil { return nil } 
+        
+        let dummyNode = ListNode(-1, head)
+        var slowPointer: ListNode? = dummyNode
+        var fastPointer = head
+        
+        var index = 1
+        while fastPointer != nil {
+            if index > n {
+                slowPointer = slowPointer?.next
+            }
+            
+            fastPointer = fastPointer?.next
+            index += 1
+        }
+        
+        slowPointer?.next =  slowPointer?.next?.next
+        
+        return dummyNode.next
+    }
+}
