2ea82992fb0b096fad5c9deda1b674af26fde463
Manish Rathi <i.am.manish.rathi@gmail.com>
2024-03-10 13:30:28 +0100

    Time: 11 ms (55.28%), Space: 16.2 MB (9.55%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.swift
@@ -0,0 +1,11 @@
+class Solution {
+    func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        var hashMap: [Int : Bool] = [:]
+        for num in nums1 { hashMap[num] = true }
+        
+        var outputHashMap: [Int: Bool] = [:]
+        for num in nums2 { if hashMap[num] != nil { outputHashMap[num] = true } }
+        
+        return Array(outputHashMap.keys)
+    }
+}
