027b83be73f8fa53a1d9fbd51ca6696e4f665151
Manish Rathi <i.am.manish.rathi@gmail.com>
2024-03-21 07:06:15 +0100

    Time: 6 ms (81.35%), Space: 16 MB (13.18%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.swift
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        return reverseList(head, nil)
+    }
+    
+    private func reverseList(_ current: ListNode?, _ previous: ListNode?) -> ListNode? {
+        if current == nil { return previous }
+        
+        let next = current?.next
+        current?.next = previous
+        
+        return reverseList(next, current)
+    }
+}
