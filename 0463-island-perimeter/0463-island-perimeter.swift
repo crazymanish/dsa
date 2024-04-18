69969755b77fac4a4afdc60b7467a3775f1f4b63
Manish Rathi <i.am.manish.rathi@gmail.com>
2024-04-18 07:15:14 +0200

    Time: 140 ms (46.88%), Space: 15.6 MB (59.38%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.swift
@@ -0,0 +1,36 @@
+class Solution {
+    func islandPerimeter(_ grid: [[Int]]) -> Int {
+        /*
+        Logic:
+        1. loop over the matrix and count the number of islands;
+        2. if the current dot is an island, count if it has any right neighbour or down neighbour;
+        3. the result is islands * 4 - neighbours * 2
+        */
+        
+        var islands = 0
+        var neighbours = 0
+        
+        let lastRowIndex = grid.count-1
+        let lastColumnIndex = grid[0].count-1
+        
+        for rowIndex in 0...lastRowIndex {
+            for columnIndex in 0...lastColumnIndex {
+                if grid[rowIndex][columnIndex] == 1 { 
+                    islands += 1 // Found island
+                    
+                    // Check for right neighbour
+                    if columnIndex+1 <= lastColumnIndex && grid[rowIndex][columnIndex+1] == 1 {
+                        neighbours += 1 // Found island in right neighbour also
+                    }
+                    
+                    // Check for down neighbour
+                    if rowIndex+1 <= lastRowIndex && grid[rowIndex+1][columnIndex] == 1 {
+                        neighbours += 1 // Found island in down neighbour also
+                    }
+                } 
+            }
+        }
+        
+        return islands * 4 - neighbours * 2
+    }
+}
