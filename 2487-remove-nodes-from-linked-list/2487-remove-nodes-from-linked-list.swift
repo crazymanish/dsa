89a0a8f894080c3f9bb62f9bf6f14611c7dd0069
Manish Rathi <i.am.manish.rathi@gmail.com>
2024-05-06 09:10:04 +0200

    Time: 683 ms (92.86%), Space: 30.2 MB (7.14%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.swift
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func removeNodes(_ head: ListNode?) -> ListNode? {
+        guard let head else { return nil }
+        
+        head.next = removeNodes(head.next)
+        
+        if head.val < (head.next ?? head).val {
+            return head.next
+        } else {
+            return head
+        }
+    }
+}
