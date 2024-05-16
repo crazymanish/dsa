cea804b44de6c018990a361839e785336f1c1d9c
Manish Rathi <i.am.manish.rathi@gmail.com>
2024-05-16 16:51:02 +0200

    Time: 26 ms (90.00%), Space: 15.7 MB (30.00%) - LeetHub

---
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.swift
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func evaluateTree(_ root: TreeNode?) -> Bool {
+        guard let root, root.val > 1 else { return root?.val == 1  }
+        let left = evaluateTree(root.left)
+        let right = evaluateTree(root.right)
+        return root.val == 2 ? left || right : left && right
+    }
+}
