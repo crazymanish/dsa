c8af88a38de20589ae119832cb9d1a4f028cf28d
Manish Rathi <i.am.manish.rathi@gmail.com>
2024-08-03 10:59:55 +0200

    Time: 19 ms (81.25%), Space: 16.3 MB (12.50%) - LeetHub

---
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.swift
@@ -0,0 +1,34 @@
+class Solution {
+    func canBeEqual(_ target: [Int], _ arr: [Int]) -> Bool {
+        var hashMap: [Int : Int] = [:]
+        
+        for number in arr { 
+            if let hashMapValue = hashMap[number] {
+                hashMap[number] = hashMapValue + 1 
+            } else {
+                hashMap[number] = 1    
+            }
+        }
+        
+        var canBeEqual = true
+        for number in target {
+            if let hashMapValue = hashMap[number] {
+                hashMap[number] = hashMapValue - 1
+            } else {
+                canBeEqual = false
+                break            
+            }
+        }
+        
+        if canBeEqual {
+            for (_, value) in hashMap {
+                if value != 0 {
+                    canBeEqual = false
+                    break            
+                }
+            }
+        }
+        
+        return canBeEqual
+    }
+}
