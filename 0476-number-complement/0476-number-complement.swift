ef5c7ab4a026e2a4f0504e326d67fa6d4ca9aa57
Manish Rathi <i.am.manish.rathi@gmail.com>
2024-08-22 15:38:10 +0200

    Time: 0 ms (100.00%), Space: 15.7 MB (22.22%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.swift
@@ -0,0 +1,11 @@
+class Solution {
+    func findComplement(_ num: Int) -> Int {
+        var output: String = ""
+        
+        for x in String(num, radix: 2) {
+            output += x == "0" ? "1" : "0"
+        }
+        
+        return Int(output, radix: 2) ?? 0
+    }
+}
