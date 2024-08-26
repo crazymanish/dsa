be3c2a5c635d2185a86011f3b2d92c2a77401432
Manish Rathi <i.am.manish.rathi@gmail.com>
2024-08-26 09:24:21 +0200

    Time: 23 ms (90.00%), Space: 17.5 MB (53.33%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.swift
@@ -0,0 +1,26 @@
+/**
+ * Definition for a Node.
+ * public class Node {
+ *     public var val: Int
+ *     public var children: [Node]
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.children = []
+ *     }
+ * }
+ */
+
+class Solution {
+    func postorder(_ root: Node?) -> [Int] {
+    	guard let currentNode = root else { return [] }
+        
+        var childrenPostorder: [Int] = []
+        for childNode in currentNode.children {
+            let childNodePostorder = postorder(childNode)
+            
+            childrenPostorder += childNodePostorder
+        }
+        
+        return childrenPostorder + [currentNode.val]
+    }
+}
