56341de6cab7144eb776ce5b43a20348fbfce1a1
Manish Rathi <i.am.manish.rathi@gmail.com>
2024-12-29 11:58:39 +0100

    Time: 467 ms (0%), Space: 25.4 MB (0%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.swift
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.swift
@@ -1,54 +1,32 @@
 class Solution {
     func numWays(_ words: [String], _ target: String) -> Int {
-        let modulo = Int(1e9 + 7)
-        var target = Array(target)
+        let MOD = 1_000_000_007
+        let (wordCount, targetCount) = (words[0].count, target.count)
 
-        let n = words[0].count
-        let k = target.count
-
-        // charArray[i][j] means, how many char i at word[j] in words
-        var charArray = Array(
-            repeating: Array(repeating: 0, count: n),
-            count: 26
-        )
-
-        let characterA = Character("a").asciiValue!
+        // 1. Create array of dictionaries to determine how many of a character
+        // are at an index.
+        var freq = Array(repeating: [Character: Int](), count: wordCount)
         for word in words {
-            var wordArray = Array(word)
-            for i in 0..<n {
-                let index = Int(wordArray[i].asciiValue! - characterA)
-                charArray[index][i] += 1
+            for (i, char) in word.enumerated() {
+                freq[i][char, default: 0] += 1
             }
         }
+        
+        // 2. Initialize 2D dp array, setting the first column to 1. Here, dp[i][j]
+        // should contain the number of ways that target[i] can be formed via
+        // index j of all words.
+        var dp = [[Int]](repeating: [Int](repeating: 0, count: targetCount+1), count: wordCount+1)
+        for i in 0..<wordCount+1 { dp[i][0] = 1 }
 
-        // dp[i][j], number of ways use 0...i index to form target[0...k]
-        var dp = Array(
-            repeating: Array(repeating: 0, count: k),
-            count: n
-        )
-
-		// index to help quick find next starting of i
-        var index = 0
-        for j in 0..<k {
-            var temp = -1
-            for i in index..<n {
-                // print("i", i)
-                let c = Int(target[j].asciiValue! - characterA)
-                let val = charArray[c][i]
-                if val > 0, temp == -1 {
-                    temp = i
-                }
-                
-                dp[i][j] = (dp[i][j] + (i > 0 ? dp[i-1][j] : 0) + val * (i > 0 && j > 0 ? dp[i-1][j-1] : 1)) % modulo
+        // 3. dp[i][j] = freq * dp[i-1][j-1] + dp[i-1][j]
+        let targetArr = Array(target)
+        for i in 1..<wordCount+1 {
+            for j in 1..<targetCount+1 {
+                let charFreq = freq[i-1][targetArr[j-1], default: 0]
+                dp[i][j] = (charFreq * dp[i-1][j-1] + dp[i-1][j]) % MOD
             }
-
-            // temp == -1 means cannot find next proper index
-            if temp == -1 { return 0 }
-            index = temp + 1
-            // print(index, j )
         }
-
-        // print(dp)
-        return dp[n-1][k-1]
+        
+        return dp[wordCount][targetCount]
     }
 }
