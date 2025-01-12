87363971f74db3b511f5ad80b8f6b4a72326d831
Manish Rathi <i.am.manish.rathi@gmail.com>
2025-01-12 09:08:34 +0100

    Time: 88 ms (100%), Space: 22.2 MB (100%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.swift
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.swift
@@ -1,31 +1,35 @@
 class Solution {
     func canBeValid(_ s: String, _ locked: String) -> Bool {
-        guard s.count % 2 == 0 else { return false }
+        let n = s.count
+        // If the length of s is odd, it cannot be a valid parentheses string
+        if n % 2 != 0 { return false }
         
         let sArray = Array(s)
         let lockedArray = Array(locked)
         
-        //forward
+        // Forward pass to check balance from left to right
         var balance = 0
-        for index in 0..<sArray.count {
-            if sArray[index] == ")" && lockedArray[index] == "1" {
-                balance -= 1
-            } else {
+        for i in 0..<n {
+            if lockedArray[i] == "0" || sArray[i] == "(" {
                 balance += 1
+            } else {
+                balance -= 1
             }
             
+            // If balance is negative, we have more closing than opening parentheses
             if balance < 0 { return false }
         }
         
-        //backward
+        // Backward pass to check balance from right to left
         balance = 0
-        for index in (0..<sArray.count).reversed() {
-            if sArray[index] == "(" && lockedArray[index] == "1" {
-                balance -= 1
-            } else {
+        for i in (0..<n).reversed() {
+            if lockedArray[i] == "0" || sArray[i] == ")" {
                 balance += 1
+            } else {
+                balance -= 1
             }
             
+            // If balance is negative, we have more opening than closing parentheses
             if balance < 0 { return false }
         }
         
