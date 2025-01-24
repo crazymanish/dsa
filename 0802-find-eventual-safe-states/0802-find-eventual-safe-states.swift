403c7b6df16e3aa63bbdc4761d9558969dfa8740
Manish Rathi <i.am.manish.rathi@gmail.com>
2025-01-24 09:27:20 +0100

    Time: 5 ms (100%), Space: 22.1 MB (9.09%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.swift
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.swift
@@ -1,4 +1,5 @@
 class Solution {
+    // Enum to represent the state of each node
     enum NodeState: Int {
         case unknown = 0
         case unsafe = 1
@@ -7,28 +8,36 @@ class Solution {
     
     func eventualSafeNodes(_ graph: [[Int]]) -> [Int] {
         let size = graph.count
+        // Array to store the state of each node
         var colour = Array<NodeState>(repeating: .unknown, count: size)
         
-        
+        // Helper function to traverse the graph and determine if a node is safe
         func traverse(index: Int) -> Bool {
+            // If the node state is already known, return if it's safe
             if colour[index] != .unknown {
                 return colour[index] == .safe
             }
         
+            // Mark the node as unsafe initially
             colour[index] = .unsafe
         
-            for i in 0..<graph[index].count {
-                if colour[graph[index][i]]  == .unsafe || traverse(index: graph[index][i]) == false {
+            // Traverse all adjacent nodes
+            for neighbor in graph[index] {
+                // If any adjacent node is unsafe or leads to an unsafe node, return false
+                if colour[neighbor] == .unsafe || !traverse(index: neighbor) {
                     return false
                 }
             }
             
+            // Mark the node as safe if all adjacent nodes are safe
             colour[index] = .safe
             return true
         }
         
         var result = [Int]()
+        // Check each node in the graph
         for i in 0..<size {
+            // If the node is safe, add it to the result
             if traverse(index: i) {
                 result.append(i)
             }
