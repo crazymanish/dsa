4cadeaf24646560f77cf770dd845e6ce8be731fb
Manish Rathi <i.am.manish.rathi@gmail.com>
2025-02-05 09:38:22 +0100

    Time: 0 ms (100%), Space: 19.6 MB (44.44%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.swift
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.swift
@@ -1,61 +1,33 @@
 class Solution {
     func areAlmostEqual(_ s1: String, _ s2: String) -> Bool {
-        let hashMapS1 = hashMap(s1)
-        let hashMapS2 = hashMap(s2)
+        // If the lengths are not equal, return false
+        if s1.count != s2.count { return false }
         
-        var areAlmostEqual = true
+        // Convert strings to arrays of characters for easy indexing
+        let chars1 = Array(s1)
+        let chars2 = Array(s2)
         
-        var hashMapDiff: [Character : Character] = [:]
-        for (key, value1) in hashMapS1 {
-            let value2 = hashMapS2[key]!
-            
-            if value1 != value2 {
-                if hashMapDiff[value1] != nil {
-                    areAlmostEqual = false
-                    break
-                } else {
-                    hashMapDiff[value1] = value2   
-                }
+        // Initialize an array to store the indices where characters differ
+        var differingIndices = [Int]()
+        
+        // Iterate through the strings to find differing indices
+        for i in 0..<chars1.count {
+            if chars1[i] != chars2[i] {
+                differingIndices.append(i)
             }
         }
         
-        if areAlmostEqual {
-            let diffKeys = Array(hashMapDiff.keys)
+        // If there are no differences, the strings are already equal
+        if differingIndices.isEmpty { return true }
         
-            if diffKeys.count == 0 { areAlmostEqual = true }
-            else if diffKeys.count == 2 {
-                let firstKey = diffKeys[0]
-                let secondKey = diffKeys[1]
-            
-                let firstValue = hashMapDiff[firstKey]!
-                let secondValue = hashMapDiff[secondKey]!
-            
-                if firstKey == secondValue && firstValue == secondKey { 
-                    areAlmostEqual = true 
-                } else {
-                    areAlmostEqual = false
-                }
-            } else {
-                areAlmostEqual = false
-            }
+        // If there are exactly two differences, check if swapping makes the strings equal
+        if differingIndices.count == 2 {
+            let i = differingIndices[0]
+            let j = differingIndices[1]
+            return chars1[i] == chars2[j] && chars1[j] == chars2[i]
         }
         
-        return areAlmostEqual
-    }
-
-    private func hashMap(_ string: String) -> [Int : Character] {
-        var hashMap: [Int : Character] = [:]
-
-        for index in 0..<string.count {
-            hashMap[index] = string[index]
-        }
-
-        return hashMap
-    }
-}
-
-private extension StringProtocol {
-    subscript(offset: Int) -> Character {
-        self[index(startIndex, offsetBy: offset)]
+        // If there are more than two differences, return false
+        return false
     }
 }
