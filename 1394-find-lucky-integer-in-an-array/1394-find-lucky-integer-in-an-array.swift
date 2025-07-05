ab048d9b579a70fc3b1fc52a437a02c47e650f38
Manish Rathi <i.am.manish.rathi@gmail.com>
2025-07-05 11:29:47 +0200

    Time: 0 ms (100%), Space: 19.2 MB (96.3%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.swift
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.swift
@@ -1,17 +1,24 @@
+// Time Complexity: O(n)
+// Space Complexity: O(k) where k is the number of unique elements in the array
+
 class Solution {
-    func findLucky(_ nums: [Int]) -> Int {
-        var hashMap: [Int : Int] = [:]
-        
-        for num in nums {
-            let hashMapValue = hashMap[num] ?? 0
-            hashMap[num] = hashMapValue + 1
+    func findLucky(_ numbers: [Int]) -> Int {
+        // Create a frequency map of the numbers in the input array.
+        var frequencyMap: [Int: Int] = [:]
+        for number in numbers {
+            frequencyMap[number, default: 0] += 1
         }
         
-        var luckyNumber = -1
-        for (key, value) in hashMap {
-            if key == value { luckyNumber = max(luckyNumber, value) }
+        // Initialize the result to -1, indicating no lucky number found yet.
+        var largestLuckyNumber = -1
+        
+        // Iterate through the frequency map to find the largest lucky number.
+        for (number, count) in frequencyMap {
+            if number == count {
+                largestLuckyNumber = max(largestLuckyNumber, number)
+            }
         }
         
-        return luckyNumber
+        return largestLuckyNumber
     }
 }
