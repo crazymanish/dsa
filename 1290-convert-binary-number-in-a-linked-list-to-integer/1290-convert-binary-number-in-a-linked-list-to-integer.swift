7db5ef27d8f810c5061bafbc4820387959cea2f7
Manish Rathi <i.am.manish.rathi@gmail.com>
2025-07-14 13:19:36 +0200

    Time: 0 ms (100%), Space: 19.3 MB (96.67%) - LeetHub

---
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.swift
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func getDecimalValue(_ head: ListNode?) -> Int {
+        var val = 0
+        var node = head
+        
+        while node != nil {
+            val = val * 2 + node!.val
+            node = node!.next
+        }
+        
+        return val
+    }
+}
