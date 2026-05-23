e311db1921fe905f9d2727aaa9da35c37c15141b
Manish Rathi <i.am.manish.rathi@gmail.com>
2026-05-23 11:46:06 +0200

    Time: 0 ms (100%), Space: 19.3 MB (75%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.swift
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.swift
@@ -1,20 +1,39 @@
 class Solution {
+    /**
+     Problem Summary:
+     Determine whether the array was originally sorted in non-decreasing order and then rotated
+     some number of positions.
+
+     Strategy:
+     In a sorted rotated array, there can be at most one place where the order "drops",
+     meaning nums[i] > nums[i + 1]. We also compare the last element back to the first
+     element to handle the circular rotation point.
+
+     Time Complexity:
+     O(n), where n is the number of elements in nums.
+
+     Space Complexity:
+     O(1), since only a counter is used.
+     */
     func check(_ nums: [Int]) -> Bool {
-        // Initialize the count of order violations
-        var orderViolations = 0
-        
-        // Traverse the array to count the order violations
-        for i in 0..<nums.count {
-            // Compare the current element with the next element (using modulo for circular comparison)
-            if nums[i] > nums[(i + 1) % nums.count] {
-                orderViolations += 1
+        var dropCount = 0
+        let count = nums.count
+
+        for index in 0..<count {
+            let nextIndex = (index + 1) % count
+
+            // A drop means the sorted order breaks at this point.
+            // In a valid sorted-and-rotated array, this can happen at most once.
+            if nums[index] > nums[nextIndex] {
+                dropCount += 1
             }
-            
-            // If there is more than one order violation, return false
-            if orderViolations > 1 { return false }
+
+            // More than one drop means the array cannot be sorted by rotation.
+            if dropCount > 1 {
+                return false
+            }
         }
-        
-        // If there is at most one order violation, return true
+
         return true
     }
 }
