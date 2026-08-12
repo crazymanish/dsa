cb96418e2010875481481ee64c3cd502b15d18f7
Manish Rathi <i.am.manish.rathi@gmail.com>
2026-08-12 09:28:40 +0200

    Time: 90 ms (12.5%), Space: 27 MB (25%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.swift
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.swift
@@ -1,21 +1,40 @@
+/**
+ Problem Summary:
+ Find the length of the longest contiguous subarray where no number
+ appears more than `k` times.
+
+ Strategy:
+ Use a sliding window with two pointers. Expand the window by moving
+ `right`. If the frequency of the newly added number exceeds `k`,
+ move `left` forward until the window becomes valid again.
+
+ Time Complexity: O(n)
+ Space Complexity: O(n)
+ */
 class Solution {
     func maxSubarrayLength(_ nums: [Int], _ k: Int) -> Int {
-        var left = 0, right = 0
-        var count: [Int: Int] = [:] // Number: Frequency
-        var longest = Int.min
+        var leftIndex = 0
+        var longestLength = 0
+        var frequencies: [Int: Int] = [:]
 
-        while right < nums.count {
-            count[nums[right], default: 0] += 1
+        for rightIndex in nums.indices {
+            let currentNumber = nums[rightIndex]
+            frequencies[currentNumber, default: 0] += 1
 
-            while count[nums[right], default: 0] > k, left < nums.count {
-                count[nums[left], default: 0] -= 1
-                left += 1 // we move left until it satisfies numbers are less than K
+            // Shrink the window while the current number appears too often.
+            while frequencies[currentNumber, default: 0] > k {
+                let leftNumber = nums[leftIndex]
+                frequencies[leftNumber, default: 0] -= 1
+                leftIndex += 1
             }
 
-            longest = max(longest, right - left + 1)
-            right += 1 // we keep expanding the subarray
+            // The window [leftIndex...rightIndex] is valid.
+            longestLength = max(
+                longestLength,
+                rightIndex - leftIndex + 1
+            )
         }
 
-        return longest
+        return longestLength
     }
 }
